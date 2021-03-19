afeef9def47d7fc468bf20d9b78e7e78b881c475
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-03-19 15:54:52 +0100

    Inicjalizacja projektu

---
+++ Pages/Recent.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace FizzBuzz.NET.Pages
+{
+    public class RecentModel : PageModel
+    {
+        private readonly ILogger<RecentModel> _logger;
+
+        public RecentModel(ILogger<RecentModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
