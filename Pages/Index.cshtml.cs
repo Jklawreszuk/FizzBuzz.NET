8910439665814fbd77d6919ca4b6ad240ca62530
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-03-19 19:59:06 +0100

    Stworzenie logiki formularza w tym odpowiedzi na rządania użytkownika

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,16 +11,34 @@ namespace FizzBuzzNET.Pages
         
         [BindProperty(SupportsGet = true)]
         public FizzBuzz FizzBuzz { get; set; }
+        public string Result { get; set; }
 
+        private string GiveAnswer(int number){
+            string result = string.Empty;
+            if (number %3==0 ){
+                result += "fizz";
+            }
+            if (number %5==0){
+                result += "buzz";
+            }
 
+            return (result == string.Empty ? $"{number}" : result);
+        }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+
+
+        /* Note: Uznałem że nie ma potrzeby przesyłania danych metodą POST,ponieważ stworzony przeze mnie formularz nie zawiera danych wrażliwych */
         public void OnGet()
         { 
-
+            if(ModelState.IsValid)
+            {
+                Result = GiveAnswer(FizzBuzz.Number); 
+                
+            }
         }
     }
 }
