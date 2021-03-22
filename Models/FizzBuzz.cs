e3286606676a0b763bd3d7f9481294a16ed91179
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-03-22 12:28:22 +0100

    Zmiana defaultowego komunikatu "Required"

--- Models/FizzBuzz.cs
+++ Models/FizzBuzz.cs
@@ -6,7 +6,7 @@ namespace FizzBuzzNET.Models
     public class FizzBuzz
     {
         
-        [Required(),Range(1,1000, ErrorMessage = "Liczba musi być z przedziału 1-1000")]
+        [Required(ErrorMesage="Pole jest wymagane"),Range(1,1000, ErrorMessage = "Liczba musi być z przedziału 1-1000")]
         public int Number { get; set; }
         public string GetAnswer()
         {
