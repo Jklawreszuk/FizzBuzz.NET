cd1ad2220c2782bdaf6a40a8f4920f031309b73f
Jklawreszuk <31008367+Jklawreszuk@users.noreply.github.com>
2021-03-30 14:26:07 +0200

    Dodanie wymaganego konstruktora

--- Data/FizzBuzzContext.cs
+++ Data/FizzBuzzContext.cs
@@ -6,7 +6,7 @@ namespace FizzBuzzNET.Data
     public class FizzBuzzContext : DbContext
     {
         DbSet<FizzBuzz> FizzBuzzes {get;set;}
-
+        public FizzBuzzContext(DbContextOptions opt):base(opt){}
         //Fluent API - inkrementacja id
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             modelBuilder.Entity<FizzBuzz>().Property(p=>p.ID).ValueGeneratedOnAdd();
